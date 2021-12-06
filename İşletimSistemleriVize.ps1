7f0844c7047f293bfdc3a7275f429a00aa56b4f6
EmreArdicoglu <EmreArdicoglu@users.noreply.github.com>
2021-12-06 19:07:37 +0300

    İsletimSistemleri

---
+++ İşletimSistemleriVize.ps1
@@ -0,0 +1,22 @@
+﻿$arr = @() 
+
+$srvs = Get-WmiObject win32_service | Select-Object Name, ProcessId  
+
+$prt = netstat -a -n -o | ConvertFrom-String | Select-Object p2, p3, p4, p5, p6 
+
+foreach($i in $srvs){
+
+
+foreach($j in $prt){
+if($i.ProcessId -eq $j.p5)
+
+{
+write-Host ($i.Name + " " + $j.p3)
+}
+
+}
+$arr += $i 
+
+} 
+
+Get-WmiObject win32_service | Select-Object Name, ProcessId  > C:\temp\cikti.txt
